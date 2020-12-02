301fe165a8f560b135735fdf2749d2f6894b5378
V10Tur <72814308+V10Tur@users.noreply.github.com>
2020-12-02 11:38:51 +0300

    Create 09.11.2020-5.java

---
+++ 09.11.2020-5.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class afd {
+    static int fibonachi(int n){
+
+        if (n == 0){
+            return 0;
+        }
+        if (n == 1){
+            return 1;
+        }
+        else{
+            return fibonachi(n - 1) + fibonachi(n - 2);
+        }
+    }
+
+}
